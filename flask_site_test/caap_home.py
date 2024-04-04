b98c744441d589c2804e53622d6c4d95053beeb5
espkz <ellie.paek@gmail.com>
2024-04-04 17:53:28 -0400

    interface (super basic)

---
+++ flask_site_test/caap_home.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+if __name__ == '__main__':
+    app.run(debug=False)
