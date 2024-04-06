0c387ca30c434f4f031af2195801b5df175597c8
espkz <ellie.paek@gmail.com>
2024-04-06 16:27:41 -0400

    attempt at connecting backend to frontend

--- flask_site_test/caap_home.py
+++ flask_site_test/caap_home.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from CAAP import *
 
 app = Flask(__name__, template_folder='templates')
 
@@ -10,9 +11,20 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload():
-    return render_template('upload.html')
+    if request.method == 'POST':
+        pdf_file = request.files['pdfFile']
+        if pdf_file:
+            pdf_file.save('flask_site_test/uploads/' + pdf_file.filename)
+            text = process_pdf('flask_site_test/uploads/' + pdf_file.filename)
+            results = get_keywords(text)
+            return redirect(url_for('show_results', results=results))
+
+@app.route('/results')
+def results():
+    results = request.args.get('results')
+    return render_template('results.html', results=results)
 
 if __name__ == '__main__':
     app.run(debug=False)
